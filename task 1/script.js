cb7fc8a067a599d56d037e7899b27242705e329d
Oleksandr <2828@i.ua>
2019-03-19 11:40:24 +0200

    правки по коду

--- task 1/script.js
+++ task 1/script.js
@@ -1,19 +1,23 @@
 'use strict'
 
-let money, time;
-
-money = prompt('Ваш бюджет на месяц?');
-
-time = prompt('Введите дату в формате YYYY-MM-DD');
+let 
+    money = prompt('Ваш бюджет на месяц?', ''),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '')
+;
 
 let appData = {
-    budget: money,
+    budjet: money,
     timeData: time,
-    expenses: prompt('Введите обязательную статью расходов в этом месяце') + ' : ' + prompt('Во сколько обойдется?'),
     optionalExpenses: {},
     income: [],
     savings: false
 };
+
+let a1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+    a2 = prompt('Во сколько обойдется?', '')
+;
+
+appData.expenses = a1 + ' : ' + a2;
 
 console.log(appData);
 
